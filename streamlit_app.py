10e1556b54b507adf100d39857264aa7c40cc9e1
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-02-20 14:12:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,13 @@ from PIL import Image
 st.markdown('# 💊 AChEpred')
 st.info('Prediction of Acetylcholinesterase inhibitors and non-inhibitors')
 st.markdown('- [**Link to Paper**](https://peerj.com/articles/2322/)')
+
+# 1. Load dataset
+st.markdown('# 1. Load dataset')
+st.markdown('## 1. Load dataset')
+st.markdown('### 1. Load dataset')
+st.markdown('#### 1. Load dataset')
+st.info('''
+A dataset consisting of Acetylcholinesterase bioactivity data was compiled from the ChEMBL database.
+Each compounds were labeled as inhibitors (pIC50 ≥ 6) or non-inhibitors (pIC50 ≤ 5) on the basis of their bioactivity data values.
+''')
